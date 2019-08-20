671143b9791a405d88f9d2cc0f52aae862dad9c8
Sara McCombs <thesaramccombs@gmail.com>
2019-08-20 02:13:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(deli_queue)
   if deli_queue.length == 0 
     puts "The line is currently empty."
   else
-    deli_queue.each_with_index(offset = 1) do |name, index|
+    deli_queue.each_with_index(offset=1) do |name, index|
       puts "#{index}. #{name}"
     end
   end
