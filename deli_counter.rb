05ebb133b421be5ef5fc171782c410e567fd0431
Sara McCombs <thesaramccombs@gmail.com>
2019-08-20 02:31:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,5 +21,17 @@ end
 def take_a_number(current_line, new_person)
   current_line << new_person 
   puts "Welcome, #{new_person}. You are number #{current_line.length} in line."
-  
+end
+
+# method named 'now_serving'
+# method returns the next person in line, at index 0, and then removes them from the array
+# method retuns "There is nobody waiting to be served!" if the array == 0 
+
+def now_serving(current_line)
+  if current_line == 0 
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{current_line[0]}."
+    current_line.
+  end
 end
