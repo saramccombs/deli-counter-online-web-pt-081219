0d66af90496ba1041f07e98c451f5659f3788695
Sara McCombs <thesaramccombs@gmail.com>
2019-08-20 02:09:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+# method named line 
+# method returns the queue and their place in line
+# method returns "The line is currently empty" if the line is empty
+
+def line(deli_queue)
+  if deli_queue.length == 0 
+    puts ""
+  else
+  end
+end
